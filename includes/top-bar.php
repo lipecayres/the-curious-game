72428b388896f71b9a1a07870ec75a9265bb3ffa
Felipe Cayres <felipescayres@gmail.com>
2024-11-18 10:46:05 -0500

    inc components includes updates to import top page and botton page and index only main code.

--- includes/top-bar.php
+++ includes/top-bar.php
@@ -1,6 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This is a curious game test">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+        rel="stylesheet">
+    <link rel="icon" href="./images/favicon.png" type="image/png">
+    <title>The Curious Game | Who else is curious here?</title>
+</head>
+<header>
     <nav>
         <ul>
             <li><a href="index.php"><img src="./images/logo.png" alt="logo-question-dot"></a></li>
-            <li>The curious game | Test Yourself</li> 
+            <li>The curious game | Test Yourself</li>
         </ul>
     </nav>
+</header>
+
+<main>
